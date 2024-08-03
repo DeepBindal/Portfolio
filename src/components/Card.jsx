1b119f1d8ea79cd14b2595573e24b18ea2462716
Deep Bindal <deepbindal36@gmail.com>
2024-08-03 11:16:27 +0530

    bug fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card({imgUrl, skill}) {
   return (
-    <article class="p-8 m-4 rounded-xl flex flex-col items-center justify-evenly bg-gray-100/5 shadow-5xl">
+    <article class="p-8 relative duration-500 hover:[transform:rotate3d(20_,-10,_1,_50deg)] m-4 rounded-xl flex flex-col items-center justify-evenly bg-gray-100/5 shadow-5xl">
       <div class="img">
         <img
           src={imgUrl}
